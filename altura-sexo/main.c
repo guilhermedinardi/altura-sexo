8c1774f184e80154115129dd2f4440fc9e7c2f8e
Guilherme Dinardi <guilhermedinardi@macbook-pro-de-guilherme.local>
2019-03-20 17:58:13 -0300

    Initial Commit

---
+++ altura-sexo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  altura-sexo
+//
+//  Created by Guilherme Dinardi on 20/03/19.
+//  Copyright © 2019 Guilherme Dinardi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

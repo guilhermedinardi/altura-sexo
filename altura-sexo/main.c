0800684f72d7fa6db4778775a2a93364f4400b4d
Guilherme Dinardi <guilhermedinardi@macbook-pro-de-guilherme.local>
2019-03-29 18:43:30 -0300

    projeto algoritmo

--- altura-sexo/main.c
+++ altura-sexo/main.c
@@ -7,9 +7,39 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    // Declaração de Variaveis
+    float altura, peso, pesoIdeal;
+    char sexo;
+    
+    // Entrada de Dados
+    printf("\n Informe seu sexo F ou M: ");
+    scanf("%c", &sexo);
+    
+    printf("\n Informe sua altura: ");
+    scanf("%f", &altura);
+    
+    printf("\n Informe seu peso: ");
+    scanf("%f", &peso);
+    
+    // Declaração de Condicionais e Saida de Dados
+    if (sexo=='M') {
+        // Calculo do Peso Masculino
+        pesoIdeal = ((72.7 * altura) - 58);
+        printf("\n Seu sexo é masculino, portanto seu peso ideal é: %.2f", pesoIdeal);
+    } else{
+        if (sexo=='F') {
+            // Calculo do Peso Feminino
+            pesoIdeal = ((62.1 * altura) - 44.7);
+            printf("\n Seu sexo é feminino, portanto seu peso ideal é: %.2f", pesoIdeal);
+        }
+    }
+    
+    
+    printf("\n");
     return 0;
 }
